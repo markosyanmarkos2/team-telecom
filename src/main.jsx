9a8309d617152c298685808f2b9930303b413e8f
markosyanmarkos2 <markosyanmarkos2@gmail.com>
2026-06-10 01:57:38 +0400

    verjacreci kayqy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App, {LrahosSlider} from './App.jsx' // Քո գլխավոր App բաղադրիչը
-import './index.css'        // Քո գլխավոր CSS-ը
+import App, {LrahosSlider} from './App.jsx' 
+import './index.css'        
 import 'swiper/css';
 import 'swiper/css/navigation'; 
 import 'swiper/css/pagination'; 
